7188871803c704f78cde568ca40e4f43ecd042bf
AldarArmaev <armaev98@mail.ru>
2026-03-02 17:09:48 +0800

    изменен способ получения токена

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ import os
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+if not TOKEN:
+    raise ValueError("ОШИБКА: Токен бота не найден ни в .env, ни в системных переменных!")
 ADMIN_TG_ID = int(os.getenv("ADMIN_TG_ID"))
 DB_PATH = "beauty_bot.db"
