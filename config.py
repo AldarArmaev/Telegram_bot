fe968d0e4b717f174a30ad08464aca1f616cef6e
AldarArmaev <armaev98@mail.ru>
2026-03-02 17:19:35 +0800

    изменена имя переменной

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 load_dotenv()
 
 
-TOKEN = os.getenv("API_TOKEN")
+BOT_TOKEN  = os.getenv("API_TOKEN")
 
-if not TOKEN:
+if not BOT_TOKEN :
     raise ValueError("ОШИБКА: Токен бота не найден ни в .env, ни в системных переменных!")
 ADMIN_TG_ID = os.getenv("ADMIN_TG_ID")
 if not ADMIN_TG_ID:
